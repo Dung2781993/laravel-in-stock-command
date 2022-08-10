e56b873a5807bfe205b2734c3aef0304114592bf
JasonLe <Dung2781993@gmail.com>
2022-08-11 01:14:27 +1000

    Create Product and Retailer migration

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,20 +2,26 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Product;
+use App\Models\Retailer;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_the_application_returns_a_successful_response()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test */
+    public function it_checks_stock_for_products_at_retailers()
+    {
+        $switch = Product::create([
+            'name' => 'Nintendo Switch'
+        ]);
+
+        $bestBuy = Retailer::create([
+            'name' => 'Best Buy'
+        ]);
+
+        $this->assertFalse($switch->inStock());
     }
 }
